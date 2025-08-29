1ef3dfefe66321e0d35ebd8f340e9ee2c70a79eb
andreapaccagnella94 <andreapacca94@live.it>
2025-08-29 15:50:02 +0200

    first commit

---
+++ mjs/hobbies.js
@@ -0,0 +1,10 @@
+/* 
+1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
+2. Esporta la funzione dal file.
+*/
+
+function hobby(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+export default hobby;
---
+++ mjs/name.js
@@ -0,0 +1,10 @@
+/* 
+1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+2. Esporta la funzione dal file.
+*/
+
+function identity(firstName, lastName) {
+    return { firstName, lastName };
+}
+
+export default identity;
